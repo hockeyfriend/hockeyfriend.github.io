22d72981f92016a475209966552c179520ae40a2
hockeyfriend <paul.richter@spreewalddreieck.de>
2022-05-25 23:12:24 +0200

    seperate set cross function

--- assets/gamelogic.js
+++ assets/gamelogic.js
@@ -1,4 +1,6 @@
-$( '.field' ).on('Click', function(event){
+$( '.field' ).on('Click', setCross);
+
+function setCross(event){
   var actField = event.target;
   actField.setAttribute("style", 'background-image: url("pics/cross.png");');
-});
+}
