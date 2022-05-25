0d049d6dd798e4fd8a0857b90b58530edd65ce5c
hockeyfriend <paul.richter@spreewalddreieck.de>
2022-05-25 23:17:12 +0200

    test new onclick trigger

--- assets/gamelogic.js
+++ assets/gamelogic.js
@@ -1,4 +1,10 @@
+/*
 $( '.field' ).on('Click', setCross);
+*/
+/* Test paste */
+const field = document.querySelector(".field");
+
+field.addEventListener("click", setCross);
 
 function setCross(event){
   var actField = event.target;
