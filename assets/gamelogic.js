7bfcb4ed0d159dcead7cb28a8e52bce763ce0383
hockeyfriend <paul.richter@spreewalddreieck.de>
2022-05-26 13:24:50 +0200

    player change and clicked field can't be clicked again

--- assets/gamelogic.js
+++ assets/gamelogic.js
@@ -1,7 +1,25 @@
+var actPlayer = -1; // -1 = player cross 1 = player circle
+var gMatrix = [
+      null, null, null,
+      null, null, null,
+      null, null, null
+]
+
 /* Set a Cross when a field get clicked */
-$( '.field' ).on('click', setCross);
+$('.field' ).on('click', setCross);
 
 function setCross(event){
   var actField = event.target;
-  actField.setAttribute("style", 'background-image: url("pics/cross.png");');
+  var actInd = parseInt(actField.getAttribute("name"));
+  
+  if (gMatrix[actInd] == null){
+      gMatrix[actInd] = actPlayer;
+
+      if (actPlayer==-1){
+        actField.setAttribute("style", 'background-image: url("pics/cross.png");');
+      } else {
+        actField.setAttribute("style", 'background-image: url("pics/circle.png");');
+      }
+      actPlayer = actPlayer * -1;
+  } else { return }
 }
