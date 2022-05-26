e839975870b57b8262a0c16dc4f90e09342212c6
hockeyfriend <paul.richter@spreewalddreieck.de>
2022-05-26 13:38:58 +0200

    implementing checkWin function

--- assets/gamelogic.js
+++ assets/gamelogic.js
@@ -11,7 +11,8 @@ $('.field' ).on('click', setCross);
 function setCross(event){
   var actField = event.target;
   var actInd = parseInt(actField.getAttribute("name"));
-  
+  var win = false;
+
   if (gMatrix[actInd] == null){
       gMatrix[actInd] = actPlayer;
 
@@ -20,6 +21,21 @@ function setCross(event){
       } else {
         actField.setAttribute("style", 'background-image: url("pics/circle.png");');
       }
+      win = checkWin(actPlayer, gMatrix);
+      console.log(win);
       actPlayer = actPlayer * -1;
   } else { return }
+}
+
+function checkWin(symbol, array){
+  var ww = 0; // count variable for a row win
+  for (let i = 0; i < array.length; i++) {
+    if(array[i] == symbol){
+      // row win if start index can be divided by 3 and every index after that is true 
+      if (i % 3 == 0 || ww > 0){
+        ww++;
+        if(ww = 3){ return true}
+      }
+    }
+  }
 }
